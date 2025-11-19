0443a3f5f29781c1d2ec7d39de9cd0de7a41da03
manangupta12 <manangupta852@gmail.com>
2025-11-19 17:41:16 +0530

    Updated port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5173,
+    host: '0.0.0.0',
+    port: 5000,
     strictPort: true,
+    allowedHosts: true,
     hmr: {
       protocol: 'ws',
       host: 'localhost',
